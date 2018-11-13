3937500cbeaf2557098f026cdfdb591633f8d98f
Lazar Otasevic <lazar.otasevic@1und1.de>
2018-11-13 15:37:08 +0100

    no tests here

--- renameto.sh
+++ renameto.sh
@@ -1,5 +1,4 @@
 sed -i '' "s/SwiftPackageWithPod/$1/g" Package.swift
 sed -i '' "s/SwiftPackageWithPod/$1/g" Readme.md
-sed -i '' "s/SwiftPackageWithPod/$1/g" Tests/*.swift
 sed -i '' "s/SwiftPackageWithPod/$1/g" SwiftPackageWithPod.podspec
 mv SwiftPackageWithPod.podspec "$1.podspec"
