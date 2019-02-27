fa22b36a467152e54d3947a14c2ecd8cb3a3023b
Lazar Otasevic <lazar.otasevic@1und1.de>
2019-02-27 18:25:19 +0100

    updated

--- Package.swift
+++ Package.swift
@@ -1,16 +1,19 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftPackageWithPod",
+    platforms: [
+        .macOS(.v10_13), .iOS("10.3")
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftPackageWithPod",
             targets: ["SwiftPackageWithPod"]),
-    ],
+        ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -21,6 +24,10 @@ let package = Package(
         .target(
             name: "SwiftPackageWithPod",
             dependencies: [],
-            path: "Sources")
-    ]
+            path: "Sources"),
+        .testTarget(
+            name: "Tests",
+            dependencies: ["SwiftPackageWithPod"],
+            path: "Tests"),
+        ]
 )
--- SwiftPackageWithPod.podspec
+++ SwiftPackageWithPod.podspec
@@ -14,7 +14,11 @@ Pod::Spec.new do |s|
     s.social_media_url = 'https://twitter.com/redhotbits'
     s.documentation_url = "https://raw.githubusercontent.com/sisoje/#{s.name}/master/README.md"
 
-    s.swift_version = '4.2'
-    s.platforms = { :ios => "10.0", :watchos => "3.0", :tvos => "10.0", :osx => "10.12" }
+    s.swift_version = '5.0'
+    s.platforms = { :ios => "10.3", :watchos => "3.0", :tvos => "10.0", :osx => "10.12" }
     s.source_files = 'Sources/**/*'
+    s.test_spec 'Tests' do |test_spec|
+        test_spec.requires_app_host = false
+        test_spec.source_files = 'Tests/**/*'
+    end
 end
